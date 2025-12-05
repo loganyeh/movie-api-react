5085a2f9ed2a0e1168bec1ff3f23b53b44b17620
loganyeh <yehlogan@gmail.com>
2025-12-05 18:19:43 -0500

    feat: navbar layout

--- src/component/NavBar.jsx
+++ src/component/NavBar.jsx
@@ -2,7 +2,13 @@
 function NavBar() {
   return (
     <>
-      <div className="h-full w-2/12 border-2 border-red-600"></div>
+      <div className="h-full w-2/12 grid grid-rows-12 grid-cols-12 border-2 border-red-600">
+            <input className="row-start-2 row-end-3 col-start-2 col-end-12 border-2 border-red-600" type="text" placeholder="Search for Movie..." />
+            <button className="row-start-3 row-end-4 col-start-2 col-end-12 border-2 border-red-600 hover:bg-gray-200 active:bg-gray-300">Search</button>
+            <div className="row-start-5 row-end-6 col-start-2 col-end-12 border-2 border-red-600">Home</div>
+            <div className="row-start-7 row-end-8 col-start-2 col-end-12 border-2 border-red-600">Favorites</div>
+            <div className="row-start-9 row-end-10 col-start-2 col-end-12 border-2 border-red-600">Info</div>
+      </div>
     </>
   );
 }
