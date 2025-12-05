c922c0ce467d01da049a949a5ac93bbfd27813a5
loganyeh <yehlogan@gmail.com>
2025-12-05 18:03:30 -0500

    feat: movies and navbar border created

---
+++ src/component/Movies.jsx
@@ -0,0 +1,12 @@
+
+function Movies() {
+
+    return (
+        <>
+            <div className="h-full w-10/12 border-2 border-red-600"></div>
+        
+        </>
+    )
+}
+
+export default Movies;
---
+++ src/component/NavBar.jsx
@@ -0,0 +1,10 @@
+
+function NavBar() {
+  return (
+    <>
+      <div className="h-full w-2/12 border-2 border-red-600"></div>
+    </>
+  );
+}
+
+export default NavBar;
