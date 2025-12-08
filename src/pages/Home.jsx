00a2e9b77dccba0eeca1fb5264c6c56f242f0cd2
loganyeh <yehlogan@gmail.com>
2025-12-08 18:49:00 -0500

    feat: fixed api

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,34 +3,20 @@ import { getMovieAPIData } from "../services/api";
 import { useEffect, useState } from "react";
 
 function Home() {
-  // const [movieArr, setMovieArr] = useState([]);
   const [movie, setMovie] = useState(null);
 
-  // useEffect(() => {
-  //     const movieAPIData = async () => {
-  //         try {
-  //             const movies = await getMovieAPIData();
-  //             setMovieArr(movies[0]);
-  //             console.log(movies[0]);
-  //         } catch (error) {
-  //             console.error(error);
-  //         }
-  //     }
-
-  //     movieAPIData();
-  // }, []);
-
   useEffect(() => {
-    const fetchMovie = async () => {
+    const movieAPIData = async () => {
       try {
         const movies = await getMovieAPIData();
-        setMovie(movies[0]); // first movie
+        setMovie(movies[0]);
+        console.log(movies[0]);
       } catch (error) {
         console.error(error);
       }
     };
 
-    fetchMovie();
+    movieAPIData();
   }, []);
 
   // console.log(movieArr.original_title);
@@ -39,7 +25,6 @@ function Home() {
     <>
       <div className="h-full w-10/12 flex flex-wrap flex-row border-2 border-red-600">
         {/* MOVIE POSTER DESIGN */}
-        {/* <MovieCard movie={movieArr}/> */}
         <div className="container">{movie && <MovieCard movie={movie} />}</div>
       </div>
     </>
