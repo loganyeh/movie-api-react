1edba8697b532b656b19141fd53bbbad88dfc993
loganyeh <yehlogan@gmail.com>
2025-12-08 17:15:11 -0500

    fix: api and MovieCard

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,21 +3,29 @@ import { getMovieAPIData } from "../services/api";
 import { useEffect, useState } from "react";
 
 function Home() {
+    const [movieArr, setMovieArr] = useState([]);
 
     useEffect(() => {
         const movieAPIData = async () => {
-            const movies = await getMovieAPIData();
-            
+            try {
+                const movies = await getMovieAPIData();
+                setMovieArr(movies[0]);
+                console.log(movies[0]);
+            } catch (error) {
+                console.error(error);
+            }
         }
 
         movieAPIData();
     }, []);
 
+    // console.log(movieArr.original_title);
+
     return (
         <>
             <div className="h-full w-10/12 flex flex-wrap flex-row border-2 border-red-600">
                 {/* MOVIE POSTER DESIGN */}
-                <MovieCard />
+                <MovieCard movie={movieArr}/>
             </div>
         </>
     )
