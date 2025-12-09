d783cc09b0027d1a808b288bcb260081dae31dd7
loganyeh <yehlogan@gmail.com>
2025-12-08 19:43:52 -0500

    broken

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -4,11 +4,11 @@ function MovieCard({movie}) {
     return(
         <>
             <div className="h-1/2 w-84.5 m-1 grid grid-rows-12 grid-cols-12 border-2 border-black rounded-2xl">
-                <img className="h-full w-full row-start-1 row-end-11 col-start-1 col-end-13 object-cover" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie1" />
+                <img className="h-full w-full row-start-1 row-end-11 col-start-1 col-end-13 object-cover rounded-t-2xl" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt="movie1" />
                 <div className="flex row-start-11 row-end-13 col-start-1 col-end-13">
                     <div className="h-full w-3/6 flex justify-center items-center">{movie.title}</div>
                     <div className="h-full w-2/6 flex justify-center items-center">{movie.vote_average.toFixed(1)}</div>
-                    <div className="h-full w-1/6 flex justify-center items-center text-4xl hover:bg-gray-200 active:bg-gray-400 active:text-red-600">o</div>
+                    <div className="h-full w-1/6 flex justify-center items-center text-4xl hover:bg-gray-200 active:bg-gray-400 active:text-red-600 rounded-br-2xl">o</div>
                 </div>
             </div>
         </>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,25 +4,44 @@ import { useEffect, useState } from "react";
 
 function Home() {
   const [movie, setMovie] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const movieAPIData = async () => {
       try {
         const movies = await getMovieAPIData();
         setMovie(movies);
-      } catch (error) {
-        console.error(error);
+      } catch (err) {
+        console.log(err);
+        setError("Failed to load movies...");
+      } finally {
+        setLoading(false);
       }
     };
 
     movieAPIData();
   }, []);
 
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
+
   return (
     <>
       <div className="h-full w-10/12 flex flex-wrap flex-row border-2 border-red-600">
         {/* MOVIE POSTER DESIGN */}
-        {movie.map((movie, index) => {return <MovieCard key={index} movie={movie} />})}
+
+        {error && <div className="error-message">{error}</div>}
+
+        {loading ? (
+          <div className="loading">Loading...</div>
+        ) : (
+          <div className="movies-grid">
+            {movie.map((movie) => (
+              <MovieCard movie={movie} key={movie.id} />
+            ))}
+          </div>
+        )}
       </div>
     </>
   );
